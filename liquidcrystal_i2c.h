4abc0010674591e2173c52e33f809e24c69be6b7
William Redenbaugh <wmredenbaugh@gmail.com>
2023-03-28 16:31:55 -0700

    working on this sort've but probably not high priority for now

---
+++ liquidcrystal_i2c.h
@@ -0,0 +1,22 @@
+#ifndef _LIQUIDCRYSTAL_I2C_H
+#define _LIQUIDCRYSTAL_I2C_H
+
+typedef struct LiquidCrystalI2C_t{
+    uint8_t _addr;
+	uint8_t _displayfunction;
+	uint8_t _displaycontrol;
+	uint8_t _displaymode;
+	uint8_t _cols;
+	uint8_t _rows;
+	uint8_t _charsize;
+	uint8_t _backlightval;
+}LiquidCrystalI2C_t;
+
+
+bool liquid_crystal_i2c_init(LiquidCrystalI2C_t *mod, uint8_t lcd_addr, uint8_t lcd_cols, uint8_t lcd_rows);
+bool liquid_crystal_i2c_begin(LiquidCrystalI2C_t *mod);
+bool liquid_crystal_i2c_clear(LiquidCrystalI2C_t *mod);
+bool1 liquid_crystal_i2c_home(LiquidCrystalI2C_t *mod);
+int liquid_crystal_i2c_no_display(LiquidCrystalI2C_t *mod);
+int liquid_crystal_i2c_
+#endif
